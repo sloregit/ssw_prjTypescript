03eef7db77956713cea65ea43be62fccc07a1d76
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-04-09 22:52:17 +0200

    4 files modified

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-9adckp)
 
+Realizzare un observable che realizzi una operazione di Get della API del servizio KVaaS utilizzando una chiave contenuta in un box di input.
+La funzionalità è collegata ad un bottone, e la stampa del valore collegato compare in un div del browser.
+La chiave: e3ce9512 (only x questo esercizio)
--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
+import { Observable, Subscriber } from ’rxjs’;
 // Import stylesheets
 import './style.css';
 //e3ce9512
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 
-const getKeyButton: HTMLElement = document.getElementById('getKey');
-const parShowKey: HTMLElement = document.getElementById('showKey');
+const getValueButton: HTMLElement = document.getElementById('getValue');
+const inputKey: HTMLElement = document.getElementById('inputKey');
+const parShowValue: HTMLElement = document.getElementById('showValue');
 
+let t = 'prova';
 
+const Obs = new Observable()
