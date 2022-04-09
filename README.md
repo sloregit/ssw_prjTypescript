1762c15d0bdec2cc67e78da29584f77fb874eccb
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-04-10 01:47:17 +0200

    4 files modified

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # typescript-9adckp
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-9adckp)
-
+STEP 1
 Realizzare un observable che realizzi una operazione di Get della API del servizio KVaaS utilizzando una chiave contenuta in un box di input.
 La funzionalità è collegata ad un bottone, e la stampa del valore collegato compare in un div del browser.
 La chiave: e3ce9512 (only x questo esercizio)
+
+STEP 2
+Realizzare una set collegata ad una casella di input in cui inserire il testo desiderato
--- index.ts
+++ index.ts
@@ -4,32 +4,26 @@ import { AjaxCreationMethod } from 'rxjs/internal/ajax/ajax';
 // Import stylesheets
 import './style.css';
 //e3ce9512
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
 
-const getValueButton: HTMLElement = document.getElementById('getValue');
 const inputKey: HTMLElement = document.getElementById('inputKey');
+const getValueButton: HTMLElement = document.getElementById('getValue');
 const parShowValue: HTMLElement = document.getElementById('showValue');
+
+const setValueButton: HTMLElement = document.getElementById('setValue');
 const inputValue: HTMLAppletElement = document.getElementById('insertValue');
+console.log(inputValue)  
+//I dati per la chiamata ajax
 const URL: string =
   'https://eu-central-1.aws.data.mongodb-api.com/app/kvaas-giwjg/endpoint/get?key=';
-
-/*const ObsKey: Observable<Event> = fromEvent(inputKey, 'input');
-ObsInput.subscribe({
-  next: (val) => {
-    console.log();
-  },
-});
-*/
 const request: AjaxRequest = {
   url: URL + 'e3ce9512',
   crossDomain: true,
 };
 
-//Il pulsante
-const ObsButton: Observable<Event> = fromEvent(getValueButton, 'click');
-ObsButton.subscribe({
-  next: (res) => view(),
+//Il pulsante GET
+const ObsButtonGet: Observable<Event> = fromEvent(getValueButton, 'click');
+ObsButtonGet.subscribe({
+  next: (res) => getValue(),
   error: (err: AjaxError) => {
     console.log(err);
   },
@@ -37,7 +31,7 @@ ObsButton.subscribe({
 });
 
 //la funzione chiamata genera un Observable che gestisce le chiamate Ajax
-function view() {
+function getValue() {
   const ObsGet: Observable<AjaxResponse<any>> = ajax(request);
   ObsGet.subscribe({
     next: (res: AjaxResponse<any>) => {
@@ -50,7 +44,27 @@ function view() {
   });
 }
 
+//Il pulsante SET
+const ObsButtonSet: Observable<Event> = fromEvent(setValueButton, 'click');
+ObsButtonSet.subscribe({
+  next: (res) => insertValue(),
+  error: (err: AjaxError) => {
+    console.log(err);
+  },
+  complete: () => {},
+});
 
-//Il box di Input
+function insertValue() {
+  const ObsSet: Observable<AjaxCreationMethod> = ajax(request);
+  ObsSet.subscribe ({
+    next: (res)
+  })
+}
 
-const ObsSet: Observable<AjaxCreationMethod> = 
+/*const ObsKey: Observable<Event> = fromEvent(inputKey, 'input');
+ObsInput.subscribe({
+  next: (val) => {
+    console.log();
+  },
+});
+*/
